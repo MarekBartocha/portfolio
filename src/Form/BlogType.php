716df2ca2a98594ced2b94a2907eb42d93e2e8ae
Marek <marek-bartocha@hotmail.com>
2025-07-15 22:14:53 +0200

    added a form for the administrator to create a topic

---
+++ src/Form/BlogType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Blog;
+use App\Entity\Topic;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content')
+            ->add('title')
+            ->add('topic', EntityType::class, [
+                'class' => Topic::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Blog::class,
+        ]);
+    }
+}
--- src/Form/TopicType.php
+++ src/Form/TopicType.php
@@ -2,28 +2,24 @@
 
 namespace App\Form;
 
-use App\Entity\Product;
+use App\Entity\Topic;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class ProductType extends AbstractType
+class TopicType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name')
-            ->add('description')
-            ->add('size')
-            ->add('save', SubmitType::class, ['label' => 'Add New Product'])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Product::class,
+            'data_class' => Topic::class,
         ]);
     }
 }
