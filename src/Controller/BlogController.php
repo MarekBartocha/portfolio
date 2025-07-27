f82399109f576be5428fc7d091d921a65d15c872
Marek <marek-bartocha@hotmail.com>
2025-07-27 15:06:49 +0200

    Blog show public done

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,16 +5,42 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\TopicRepository;
+use App\Repository\BlogRepository;
 
 final class BlogController extends AbstractController
 {
-    #[Route('/{_locale}/blog', name: 'blog_index')]
-    public function index(string $_locale): Response
+    #[Route('/{_locale}/blog-topics', name: 'admin_index')]
+    public function index(string $_locale, TopicRepository $topicRepository): Response
     {
+        $topics = $topicRepository->findAll();
+
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'controller_name' => 'AdminController',
             'current_locale' => $_locale,
-            'site' => 'blog',
+            'site' => 'blog-topics',
+            'topics' => $topics,
+        ]);
+    }
+
+    #[Route('/{_locale}/blog/{slug}', name: 'public_topic_show')]
+    public function show(string $_locale, string $slug, TopicRepository $topicRepository, BlogRepository $blogRepository): Response
+    {
+
+        $topic = $topicRepository->findOneBy(['slug' => $slug]);
+
+        if (!$topic) {
+            throw $this->createNotFoundException('Temat nie istnieje.');
+        }
+
+        $blogs = $blogRepository->findBy(['topic' => $topic]);
+
+        return $this->render('topic/public-show.html.twig', [
+            'topic' => $topic,
+            'current_locale' => $_locale,
+            'site' => 'blog/' . $slug,
+            'slug' => $slug,
+            'blogs' => $blogs,
         ]);
     }
 }
